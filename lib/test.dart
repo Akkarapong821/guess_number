215a2f3eeb31505837d5ea89e40bef4d1654e36f
Akkarapong821 <95558483+Akkarapong821@users.noreply.github.com>
2021-12-12 12:44:32 +0700

    Add files via upload

---
+++ lib/test.dart
@@ -0,0 +1,23 @@
+// top-level function
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  var i = 0;
+  var r = Random();
+  var answer = r.nextInt(10);
+  while(true){
+    stdout.write("Please guess the number: ");
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+    if(guess == answer){
+      print("CORRECT");
+      print("END PROGRAM...");
+      return;
+    } else {
+      print("TRY AGAIN");
+    }
+  }
+}
+
+// console app
