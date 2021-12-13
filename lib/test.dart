9d8fb96a741ed990c8c531d733f664a42b06233c
Akkarapong821 <95558483+Akkarapong821@users.noreply.github.com>
2021-12-14 01:27:48 +0700

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -2,20 +2,32 @@
 import 'dart:io';
 import 'dart:math';
 
-void main(){
-  var i = 0;
+void main() {
   var r = Random();
   var answer = r.nextInt(100);
-  while(true){
-    stdout.write("Please guess the number: ");
+  var count = 0;
+  while (true) {
+    stdout.write("Please guess the number between 1 to 100: ");
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
-    if(guess == answer){
-      print("CORRECT");
+
+    if (guess == answer) {
+      count++;
+      print("➜ $guess is CORRECT ❤, total guesses: $count");
       print("END PROGRAM...");
       return;
+    }
+    if (guess == null || guess >= 100 || guess <= 0) {
+      var guess = int.tryParse(input);
     } else {
-      print("TRY AGAIN");
+      if (guess > answer) {
+        print("➜ TRY AGAIN $guess is TOO HIGH! ▲");
+        count++;
+      }
+      if (guess < answer) {
+        print("➜ TRY AGAIN $guess is TOO LOW! ▼");
+        count++;
+      }
     }
   }
 }
